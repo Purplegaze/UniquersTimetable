944d26cc679c5fb434ee96731ad00d22a1d632de
mayyadow <mayadowman2024@gmail.com>
2025-11-11 14:01:14 -0500

    timetableview ui completed

--- src/main/java/view/TimetableView.java
+++ src/main/java/view/TimetableView.java
@@ -1,1 +1,55 @@
+package view;
 
+import java.awt.*;
+import javax.swing.*;
+
+public class TimetableView extends JFrame {
+
+    public TimetableView(){
+        setTitle("Weekly Timetable");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setSize(1000, 600);
+
+        String[] weeklyTimetable = {"Time", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        JPanel headerPanel = new JPanel(new GridLayout(1,weeklyTimetable.length));
+        JPanel timePanel = new JPanel();
+        JPanel timeSlots = new JPanel();
+
+        int start_time = 9;
+        int end_time = 21;
+        int num_rows = end_time-start_time+1;
+
+        timePanel.setLayout(new GridLayout(num_rows, 1));
+        timeSlots.setLayout(new GridLayout(num_rows, weeklyTimetable.length - 1));
+
+        for (String day : weeklyTimetable) {
+            headerPanel.add(new JLabel(day));
+        }
+
+        add(headerPanel, BorderLayout.NORTH);
+        add(timePanel, BorderLayout.WEST);
+        add(timeSlots, BorderLayout.CENTER);
+
+        for (int i = 9; i <= end_time; i++){
+            for (int j = 0; j < weeklyTimetable.length; j++){
+                if (j == 0){
+                    JLabel timeLabel = new JLabel( i + ":00");
+                    timePanel.add(timeLabel);
+                }
+                else {
+                    JPanel slot = new JPanel();
+                    slot.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+                    slot.setBackground(Color.WHITE);
+                    timeSlots.add(slot);
+                }
+            }
+        }
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(TimetableView::new);
+    }
+}
