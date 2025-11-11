d47429d85c360fa140b3c5f96e34b5b98363ae37
Jason D. / Purplegaze <purplecoin64@gmail.com>
2025-11-11 12:58:40 -0500

    Add TimetableBlock and part of the Timetable code (NOT DONE)

--- src/main/java/entity/Timetable.java
+++ src/main/java/entity/Timetable.java
@@ -1,1 +1,18 @@
+public class Timetable {
+    private ArrayList<Course> courses;
+    private ArrayList<TimetableBlock> blocks;
+    private ArrayList<Conflict> conflicts;
 
+    public Timetable() {
+        courses = new ArrayList<>();
+        conflicts = new ArrayList<>();
+    }
+
+    public void addSection(Section section) {
+
+    }
+
+    public void removeCourse() {
+
+    }
+}
