cd5d2df20648b43ce15cfa0cb4c02032cc0e58f0
Jason D. / Purplegaze <purplecoin64@gmail.com>
2025-11-13 14:24:49 -0500

    Add barebones Timetable and TimetableBlock classes
    Still to do (once TimeSlot is made): Prevent conflicts, figure out where travel times are to be added

--- src/main/java/entity/Timetable.java
+++ src/main/java/entity/Timetable.java
@@ -1,3 +1,7 @@
+package entity;
+
+import java.util.ArrayList;
+
 public class Timetable {
     private ArrayList<Course> courses;
     private ArrayList<TimetableBlock> blocks;
@@ -5,14 +9,53 @@ public class Timetable {
 
     public Timetable() {
         courses = new ArrayList<>();
+        blocks = new ArrayList<>();
         conflicts = new ArrayList<>();
     }
 
-    public void addSection(Section section) {
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public ArrayList<TimetableBlock> getBlocks() {
+        return blocks;
+    }
+
+    public void addSectionOfNewCourse(Section section) {
+        // TODO: Prevent conflicts from being added.
+
+        courses.add(section.getCourse());
+
+        addSection(section);
+    }
+
+    private void addSection(Section section) {
+        for (TimeSlot time : section.getTimes()) {
+            TimetableBlock tb = new TimetableBlock(section, time);
+            blocks.add(tb);
+        }
+    }
+
+    public void changeSectionOfExistingCourse(Section section) {
+        for (TimetableBlock tb : blocks) {
+            if (tb.getSection().equals(section)) {
+                blocks.remove(tb);
+            }
+        }
+        addSection(section);
 
     }
 
-    public void removeCourse() {
+    public void removeCourse(Course course) {
+        for (TimetableBlock tb : blocks) {
+            if (tb.getCourse().equals(course)) {
+                blocks.remove(tb);
+            }
+        }
+    }
 
+    public ArrayList getTravelTimes() {
+        // TODO: Figure out where travel times are to be calculated.
+        return new ArrayList();
     }
 }
