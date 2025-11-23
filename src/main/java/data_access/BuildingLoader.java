fe7b20df9c390c12226ea6fc0d384badef0f0105
mayyadow <mayadowman2024@gmail.com>
2025-11-22 22:48:41 -0500

    geocoded the longitude and latitude of all buildings

--- src/main/java/data_access/BuildingLoader.java
+++ src/main/java/data_access/BuildingLoader.java
@@ -1,28 +1,28 @@
-package data_access;
-import entity.Building;
-
-import org.json.*;
-import java.nio.file.*;
-import java.util.*;
-
-public class BuildingLoader {
-
-    public static List<Building> loadBuildings(String path) throws Exception {
-        String json = Files.readString(Paths.get(path));
-        JSONArray arr = new JSONArray(json);
-
-        List<Building> buildings = new ArrayList<>();
-
-        for (int i = 0; i < arr.length(); i++) {
-            JSONObject obj = arr.getJSONObject(i);
-
-            String code = obj.getString("code");
-            String name = obj.getString("name");
-            String address = obj.getString("address");
-
-            buildings.add(new Building(code, name, address));
-        }
-
-        return buildings;
-    }
-}
+//package data_access;
+//import entity.Building;
+//
+//import org.json.*;
+//import java.nio.file.*;
+//import java.util.*;
+//
+//public class BuildingLoader {
+//
+//    public static List<Building> loadBuildings(String path) throws Exception {
+//        String json = Files.readString(Paths.get(path));
+//        JSONArray arr = new JSONArray(json);
+//
+//        List<Building> buildings = new ArrayList<>();
+//
+//        for (int i = 0; i < arr.length(); i++) {
+//            JSONObject obj = arr.getJSONObject(i);
+//
+//            String code = obj.getString("code");
+//            String name = obj.getString("name");
+//            String address = obj.getString("address");
+//
+//            buildings.add(new Building(code, name, address));
+//        }
+//
+//        return buildings;
+//    }
+//}
