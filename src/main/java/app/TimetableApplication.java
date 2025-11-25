e2a2cd024cd149243c3062d259a634035adcb192
Xiaohan M. <xiaohan.m9@gmail.com>
2025-11-25 01:58:55 -0500

    Update TimetableApplication

--- src/main/java/app/TimetableApplication.java
+++ src/main/java/app/TimetableApplication.java
@@ -1,16 +1,23 @@
 package app;
 
 import data_access.CourseDataAccessInterface;
-import data_access.InMemoryCourseDataAccess;
+import data_access.JSONCourseDataAccess;
+import data_access.InMemoryTimetableDataAccess;
+import data_access.TimetableDataAccessInterface;
 import entity.Course;
 import usecase.search.SearchCourseInputBoundary;
 import usecase.search.SearchCourseInputData;
 import usecase.search.SearchCourseOutputBoundary;
 import usecase.search.SearchCourseOutputData;
 import usecase.search.SearchCourseInteractor;
+import usecase.addcourse.AddCourseInputBoundary;
+import usecase.addcourse.AddCourseOutputBoundary;
+import usecase.addcourse.AddCourseOutputData;
+import usecase.addcourse.AddCourseInteractor;
 import view.MainView;
 import view.SearchPanel;
 import view.SectionView;
+import view.TimetableView;
 
 import javax.swing.*;
 import java.util.ArrayList;
@@ -26,23 +33,90 @@ public class TimetableApplication {
 
             MainView mainView = new MainView();
             SearchPanel searchPanel = mainView.getSearchPanel();
+            TimetableView timetableView = mainView.getTimetableView();
 
             // Data Access
-            CourseDataAccessInterface courseDataAccess = new InMemoryCourseDataAccess();
+            CourseDataAccessInterface courseDataAccess = new JSONCourseDataAccess();
+            TimetableDataAccessInterface timetableDataAccess = new InMemoryTimetableDataAccess();
 
-            SearchCourseInputBoundary interactor = createSearchCourseInteractor(searchPanel, courseDataAccess);
+            CourseColorHelper colorHelper = new CourseColorHelper();
 
-            connectSearchPanel(searchPanel, interactor, courseDataAccess);
+            AddCourseInputBoundary addCourseInteractor = createAddCourseInteractor(
+                    timetableDataAccess, timetableView, colorHelper);
 
-            interactor.execute(new SearchCourseInputData(""));
+            SearchCourseInputBoundary searchInteractor = createSearchInteractor(
+                    courseDataAccess, searchPanel);
+
+            connectSearchPanel(searchPanel, searchInteractor, courseDataAccess, addCourseInteractor);
+
+            searchInteractor.execute(new SearchCourseInputData(""));
 
             mainView.display();
         });
     }
 
+    private static AddCourseInputBoundary createAddCourseInteractor(
+            TimetableDataAccessInterface timetableDataAccess,
+            TimetableView timetableView,
+            CourseColorHelper colorHelper) {
+
+        AddCourseOutputBoundary presenter = new AddCourseOutputBoundary() {
+            @Override
+            public void presentSuccess(AddCourseOutputData outputData) {
+                displayCourseOnTimetable(timetableView, outputData, colorHelper, false);
+                timetableView.showSuccessMessage(
+                        outputData.getCourseCode(),
+                        outputData.getSectionCode()
+                );
+            }
+
+            @Override
+            public void presentConflict(AddCourseOutputData outputData) {
+                String conflictMsg = "Conflict with: " + String.join(", ", outputData.getConflictingCourses());
+                timetableView.showConflictWarning(conflictMsg);
+                displayCourseOnTimetable(timetableView, outputData, colorHelper, true);
+            }
+
+            @Override
+            public void presentError(String errorMessage) {
+                timetableView.showErrorMessage(errorMessage);
+            }
+        };
+
+        return new AddCourseInteractor(timetableDataAccess, presenter);
+    }
+
+
+
+    /**
+     * Display a course on the timetable.
+     */
+    private static void displayCourseOnTimetable(
+            TimetableView timetableView,
+            AddCourseOutputData outputData,
+            CourseColorHelper colorHelper,
+            boolean hasConflict) {
+
+        TimetableView.TimetableSlotItem item = new TimetableView.TimetableSlotItem(
+                outputData.getCourseCode(),
+                outputData.getSectionCode(),
+                outputData.getLocation(),
+                colorHelper.getNextColor(),
+                hasConflict
+        );
+
+        timetableView.displayCourse(
+                outputData.getDay(),
+                outputData.getStartHour(),
+                outputData.getEndHour(),
+                item
+        );
+    }
+
     private static void connectSearchPanel(SearchPanel searchPanel,
                                            SearchCourseInputBoundary interactor,
-                                           CourseDataAccessInterface courseDataAccess) {
+                                           CourseDataAccessInterface courseDataAccess,
+                                           AddCourseInputBoundary addCourseInteractor) {
 
         searchPanel.setListener(new SearchPanel.SearchPanelListener() {
             @Override
@@ -54,23 +128,23 @@ public class TimetableApplication {
             public void onResultSelected(String courseCode) {
                 Course course = courseDataAccess.findByCourseCode(courseCode);
                 if (course != null) {
-                    new SectionView(course);
+                    new SectionView(course, addCourseInteractor);
                 }
             }
         });
     }
 
-    private static SearchCourseInputBoundary createSearchCourseInteractor(SearchPanel searchPanel, CourseDataAccessInterface courseDataAccess) {
+    private static SearchCourseInputBoundary createSearchInteractor(
+            CourseDataAccessInterface courseDataAccess,
+            SearchPanel searchPanel) {
+
         SearchCourseOutputBoundary presenter = new SearchCourseOutputBoundary() {
             @Override
             public void presentSearchResults(SearchCourseOutputData outputData) {
                 List<SearchPanel.SearchResultItem> viewModels = new ArrayList<>();
                 for (SearchCourseOutputData.ResultItem item : outputData.getResults()) {
                     String displayText = item.getCourseCode() + " - " + item.getCourseName();
-                    viewModels.add(new SearchPanel.SearchResultItem(
-                            item.getCourseCode(),
-                            displayText
-                    ));
+                    viewModels.add(new SearchPanel.SearchResultItem(item.getCourseCode(), displayText));
                 }
                 searchPanel.displayResults(viewModels);
             }
@@ -85,6 +159,9 @@ public class TimetableApplication {
                 searchPanel.displayError(errorMessage);
             }
         };
+
         return new SearchCourseInteractor(courseDataAccess, presenter);
     }
+
+
 }
