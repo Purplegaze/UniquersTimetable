f54d6541dca5064263fea28d8de862f7e2bb0107
abdullah-abbh <abdulllah.alhidary@mail.utoronto.ca>
2025-11-24 20:20:18 -0500

    implemented FilterCoursesInputBoundary.java (CA-1/8)

---
+++ src/main/java/use_case/filter_courses/FilterCoursesInputBoundary.java
@@ -0,0 +1,7 @@
+package use_case.filter_courses;
+
+import use_case.filter_courses.FilterCoursesInputData;
+
+public interface FilterCoursesInputBoundary {
+    void execute(FilterCoursesInputData inputData);
+}
