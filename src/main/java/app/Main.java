d841d514edf83dee6db118565d1b7572fcfbcec3
abdullah-abbh <abdulllah.alhidary@mail.utoronto.ca>
2025-12-01 22:17:50 -0500

    Link the use case 2 filter feature to the main app setup

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -56,6 +56,14 @@ import view.WalkingTimeView;
 import view.WalkingTimeViewAdapter;
 import data_access.WalkingTimeDataAccessObject;
 
+import interface_adapter.filter_courses.FilterCoursesController;
+import interface_adapter.filter_courses.FilterCoursesPresenter;
+import interface_adapter.filter_courses.FilterCoursesViewModel;
+
+import usecase.filter_courses.FilterCoursesInputBoundary;
+import usecase.filter_courses.FilterCoursesInteractor;
+import usecase.filter_courses.FilterCoursesOutputBoundary;
+
 /**
  * Main entry point for the Timetable Application.
  * <p>
@@ -82,6 +90,14 @@ public class Main {
                 SearchViewModel searchViewModel = new SearchViewModel();
                 AddCourseViewModel addCourseViewModel = new AddCourseViewModel();
                 DeleteSectionViewModel deleteSectionViewModel = new DeleteSectionViewModel();
+
+                FilterCoursesViewModel filterCoursesViewModel = new FilterCoursesViewModel();
+                FilterCoursesOutputBoundary filterCoursesPresenter =
+                        new FilterCoursesPresenter(searchViewModel);
+                FilterCoursesInputBoundary filterCoursesInteractor =
+                        new FilterCoursesInteractor(courseDataAccess.getAllCourses(), filterCoursesPresenter);
+                FilterCoursesController filterCoursesController =
+                        new FilterCoursesController(filterCoursesInteractor);
 
                 // Create UI views
                 MainView mainView = new MainView();
@@ -130,6 +146,7 @@ public class Main {
 
                 searchPanel.setController(searchCourseController);
                 searchPanel.setViewModel(searchViewModel);
+                searchPanel.setFilterController(filterCoursesController);
                 timetableView.setDeleteController(deleteSectionController);
                 walkingTimeView.setWalkingController(walkingController);
                 walkingTimeView.setTimetable(timetableDataAccess.getTimetable());
